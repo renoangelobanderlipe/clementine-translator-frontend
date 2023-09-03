1d0f0e5d103cfdf429132963cd8e47dd26d5172b
Reno Angelo <personal.renoangelo@gmail.com>
2023-09-03 18:40:00 +0800

    refs #7 code cleanup

--- src/components/ContainerComponent.jsx
+++ src/components/ContainerComponent.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { SelectLanguageComponent } from './SelectLanguageComponent'
 import { Card, Textarea } from 'flowbite-react'
 
-export const ContainerComponent = ({className, disabled, value}) => {
+export const ContainerComponent = ({ className, disabled, value, setSourceLanguage, setTargetLanguage }) => {
   return (
     <Card className={`bg-[#FFFFFF33] backdrop-blur-[25px] flex flex-col gap-24 font-poppins ${className}`}>
-     <SelectLanguageComponent/>
-     <Textarea
+      <SelectLanguageComponent />
+      <Textarea
         className='resize-none h-[250px] w-[300px] disabled:opacity-100'
         id="comment"
         placeholder="Input text here"
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,83 @@
 
 
-import React, { Fragment, useEffect, useState } from 'react';
-import { fetchLanguages } from '../utils/api';
-import { Button, Card, Label, Select } from 'flowbite-react';
-import { SelectLanguageComponent } from '../components/SelectLanguageComponent';
-import { ContainerComponent } from '../components/ContainerComponent';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'flowbite-react';
+import { SourceComponent } from './../components/SourceComponent';
+import { TargetComponent } from './../components/TargetComponent';
+import { fetchLanguages, translate } from '../utils/api';
 
 const HomePage = () => {
+  const [languages, setLanguages] = useState([]);
+  const [sourceLanguage, setSourceLanguage] = useState();
+  const [targetLanguage, setTargetLanguage] = useState();
+  const [body, setBody] = useState();
+  const [translated, setTranslated] = useState('');
+
+  const getLanguages = async () => {
+    const result = await fetchLanguages();
+
+    if (result.ok) {
+      setLanguages(result.data);
+    } else {
+      console.log('Something Went Wrong!');
+    }
+  }
+
+  const handleSubmit = async (event) => {
+
+    const result = await translate({
+      'source_language': sourceLanguage.code,
+      'target_language': targetLanguage.code,
+      'body': body,
+    })
+    console.log('result', result);
+    if (result.ok) {
+      setTranslated(result.data[0]);
+      console.log('result.data.message',result.data);
+
+      event.preventDefault()
+    } else {
+      console.log('test', result.data.message);
+      event.preventDefault()
+    }
+    event.preventDefault()
+  }
+
+  useEffect(() => {
+    getLanguages();
+  }, []);
 
   return (
     <div className="bg-[url('/images/background.png')] h-screen w-screen bg-cover bg-no-repeat flex flex-col gap-24 items-center justify-center font-poppins ">
       <h1 className='text-h2 uppercase text-white'>Language Translator</h1>
-      <div className="relative flex gap-16">
-        <ContainerComponent className={'h-fit'} />
-        <Button
-          className='uppercase font-[700] bg-[#FF007A] rounded-[5px] absolute top-[300px] left-[300px] z-10 focus:ring-0'>
-          Translate</Button>
-        <ContainerComponent className='mt-[80px]' disabled={true} />
-      </div>
+      {/* <form > */}
+        <div className="relative flex gap-16">
+
+          <SourceComponent
+            className={'h-fit'}
+            languages={languages}
+            sourceLanguage={setSourceLanguage}
+            body={setBody}
+          />
+
+          <Button
+            onClick={handleSubmit}
+
+            type="submit"
+            className='uppercase font-[700] bg-[#FF007A] rounded-[5px] absolute top-[300px] left-[300px] z-10 focus:ring-0'
+          >
+            Translate
+          </Button>
+
+          <TargetComponent
+            className='mt-[80px]'
+            disabled={true}
+            languages={languages}
+            targetLanguage={setTargetLanguage}
+            translated={translated}
+          />
+        </div>
+      {/* </form> */}
     </div>
   );
 }
