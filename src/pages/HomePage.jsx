370f3edfe9658cf9f4453bdc6d493eba4f34a01a
Reno Angelo <personal.renoangelo@gmail.com>
2023-09-03 11:33:08 +0800

    initial setup

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+
+
+import React, { Fragment } from 'react';
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      Starter Template for clementine-translator-frontend
+    </Fragment>
+  );
+}
+
+export default HomePage;
+
+
+
